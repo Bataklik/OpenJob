756ff352ee64cb763be034a2fde78ee014ee8229
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-02-22 21:37:00 +0100

    feat: :sparkles: Set up FastAPI and implement endpoint to extract text from PDF

--- oj_backend/main.py
+++ oj_backend/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from oj-backend!")
+from fastapi import FastAPI
+from services.pdf_plumber import plumb_text_from_pdf
+app = FastAPI()
 
-
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def plumb_pdf():
+    text = plumb_text_from_pdf("CV-Burak-M-Balci-2025_NL.pdf")
+    return {"pdf_text": text}
