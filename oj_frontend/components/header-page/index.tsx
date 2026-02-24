d334d45c3fa984d63d99bbce0fcf5f3b47c4d51b
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-02-25 00:04:46 +0100

    style: enhance HeaderPage layout with improved styling and spacing for better visual presentation

--- oj_frontend/components/header-page/index.tsx
+++ oj_frontend/components/header-page/index.tsx
@@ -12,9 +12,18 @@ interface HeaderPageProps {
 
 export default function HeaderPage({ title, description }: HeaderPageProps) {
     return (
-        <PageHeader>
-            <PageHeaderHeading>{title}</PageHeaderHeading>
-            <PageHeaderDescription>{description}</PageHeaderDescription>
-        </PageHeader>
+        <div className="w-full py-16 rounded-2xl">
+            <PageHeader className="text-center space-y-4">
+                <PageHeaderHeading className="mx-auto max-w-2xl text-4xl font-semibold tracking-tight">
+                    {title}
+                </PageHeaderHeading>
+
+                <div className="mx-auto h-2 w-18 rounded-full bg-primary" />
+
+                <PageHeaderDescription className="mx-auto max-w-2xl text-base text-muted-foreground">
+                    {description}
+                </PageHeaderDescription>
+            </PageHeader>
+        </div>
     );
 }
