bb0e0c349e37946fa5baa554b8f6932715e95b71
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-02-27 02:00:38 +0100

    feat: implement file analysis functionality in Home component, including state management for score, matched skills, and motivation letter

--- oj_frontend/app/page.tsx
+++ oj_frontend/app/page.tsx
@@ -2,11 +2,7 @@
 import AnalysePage from "@/components/results/SkillsAnalysisCard";
 import CoverLetterCard from "@/components/results/CoverLetterCard";
 import HeaderPage from "@/components/header/HeaderSection";
-import PageCard from "@/components/layout/PageCard";
 import ScoreCard from "@/components/results/ScoreCard";
-import ScoreChart from "@/components/results/ScoreChart";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import UploadPage from "@/components/upload/UploadSection";
 import { useRef, useState } from "react";
 
@@ -14,9 +10,53 @@ export default function Home() {
     const fileInputRef = useRef<HTMLInputElement | null>(null);
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
     const [jobText, setJobText] = useState("");
-    const [score, setScore] = useState(40);
-    const missing = ["Scala", "C++", "Go"];
-    const matched = ["C#", "Java", ".Net", "Python"];
+    const [isAnalyzing, setIsAnalyzing] = useState(false);
+
+    const [score, setScore] = useState(null);
+    const [matched, setMatched] = useState<string[]>([]);
+    const [missing, setMissing] = useState<string[]>([]);
+    const [letter, setLetter] = useState(null);
+    const [matchText, setMatchText] = useState("None");
+    const handleAnalyse = () => {
+        if (!selectedFile) {
+            alert("Selecteer eerst een PDF");
+            return;
+        }
+        setIsAnalyzing(true);
+
+        const formData = new FormData();
+        formData.append("cv_file", selectedFile);
+        formData.append("vacancy_text", jobText);
+
+        fetch("http://localhost:8000/match", {
+            method: "POST",
+            body: formData,
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("Response van /match endpoint:", data);
+                setScore(data.response.match_percentage);
+                setMatched(data.response.matched_skills);
+                setMissing(data.response.missing_skills);
+                setLetter(data.response.motivation_letter);
+                setMatchText(data.response.match_text);
+            })
+            .catch((err) => {
+                console.error("Fout bij /match:", err);
+            })
+            .finally(() => {
+                setIsAnalyzing(false);
+            });
+    };
+    const handleClear = () => {
+        setSelectedFile(null);
+        setJobText("");
+        setScore(null);
+        setMatched([]);
+        setMissing([]);
+        setLetter(null);
+        setMatchText("None");
+    };
     const handleClick = () => {
         fileInputRef.current?.click();
     };
@@ -49,20 +89,18 @@ export default function Home() {
                         selectedFile={selectedFile}
                         handleClick={handleClick}
                         handleFileChange={handleFileChange}
+                        handleAnalyse={handleAnalyse}
+                        handleClear={handleClear}
+                        isAnalyzing={isAnalyzing}
                     />
 
                     <div className="flex w-full flex-col gap-4">
                         <div className="mt-10 flex w-full flex-col gap-6 text-base font-medium sm:flex-row">
-                            <ScoreCard
-                                score={score}
-                                matchText={
-                                    "Strong match for Bobba Bobba Developer."
-                                }
-                            />
+                            <ScoreCard score={score} matchText={matchText} />
                             <AnalysePage matched={matched} missing={missing} />
                         </div>
-                        <div className="">
-                            <CoverLetterCard letter={null} />
+                        <div>
+                            <CoverLetterCard letter={letter} />
                         </div>
                     </div>
                 </div>
